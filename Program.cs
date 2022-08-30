9b44fb08c5c85515f728a6f1d58e1b7ca3e75e5e
LunacyWithin <s2k-r59@mail.ru>
2022-08-31 00:01:13 +0500

    21

--- Program.cs
+++ Program.cs
@@ -11,9 +11,38 @@ namespace Unit_10._5
             myCalc myCalc = new myCalc();
             NumReader numReader = new NumReader();
 
+            int a;
+            int b;
+            int i = 1;
+            while (true && i>0)
+            {
+                try
+                {
+                    Console.WriteLine("\n Input value a");
+                    a = numReader.KeyRead();
+                    i--;
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            i++;
+            while (true && i > 0)
+            {
+                try
+                {
+                    Console.WriteLine("\n Input value b");
+                    b = numReader.KeyRead();
+                    i--;
 
-            
-
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
 
 
 
